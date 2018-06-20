955b74d42e9b566a556c5a8d9b432627931fa82c
JeraldLOakley <jerryoakley989@gmail.com>
2018-06-20 15:49:44 -0400

    fixed identity connection string

--- PokeJam/PokeJam/Models/PokeJam.Designer.cs
+++ PokeJam/PokeJam/Models/PokeJam.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Grand Circus Student\Desktop\GrandCircusRepository\C#CodeRepository\PokeJam\PokeJam\PokeJam\Models\PokeJam.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Jerald\source\repos\PokeJam\PokeJam\PokeJam\Models\PokeJam.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
