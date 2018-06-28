aedae6f74bf0201deff69afdf0f0252269eab526
Anamul Islam <anamul.islam127@gmail.com>
2018-06-28 05:28:34 -0400

    A lot of CSS and other styling

--- PokeJam/PokeJam/App_Start/BundleConfig.cs
+++ PokeJam/PokeJam/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace PokeJam
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/Pokebackground1.css",
                       "~/Content/site.css"));
         }
     }
